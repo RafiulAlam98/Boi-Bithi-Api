77b97878fca0113b327269bea3762ef083f66ddd
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-07-26 19:55:17 +0600

    HANDLED: globalerror handler and validation request

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express'
 import cors from 'cors'
 import routes from './app/routes/index'
+import { globalErrorHandler } from './app/middlewares/globalErrorHandler'
 
 const app: Application = express()
 
@@ -13,5 +14,7 @@ app.use(express.urlencoded({ extended: true }))
 
 //Application Route
 app.use('/api/v1/', routes)
+
+app.use(globalErrorHandler)
 
 export default app
