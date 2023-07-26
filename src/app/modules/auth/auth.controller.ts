a96a0418282f62d7d2d100d4cd9e18c03c633155
M Rafiul Alam Tonmoy <mrafiul.alam7@gmail.com>
2023-07-26 22:06:34 +0600

    DONE: Refresh token part

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -3,19 +3,41 @@ import { Request, Response } from 'express'
 import { catchAsync } from '../../../shared/catchAsync'
 import sendResponse from '../../../shared/sendResponse'
 import { AuthValidationService } from './auth.service'
+import config from '../../../config'
+import { IRefreshTokenResponse } from './auth.interface'
 
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body
 
   const result = await AuthValidationService.loginUser(loginData)
+  const { refreshToken, ...others } = result
+  const cookieOptions = {
+    secure: config.env === 'production',
+    httpOnly: true,
+  }
+  res.cookie('refreshToken', refreshToken, cookieOptions)
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User Logged in successfully',
-    data: result,
+    data: others,
   })
 })
 
+const refreshTokenController = catchAsync(
+  async (req: Request, res: Response) => {
+    const { refreshToken } = req.cookies
+    const result = await AuthValidationService.refreshTokenService(refreshToken)
+    sendResponse<IRefreshTokenResponse>(res, {
+      statusCode: httpStatus.OK,
+      success: true,
+      message: 'User Logged in successfully',
+      data: result,
+    })
+  },
+)
+
 export const AuthValidationController = {
   loginUser,
+  refreshTokenController,
 }
--- src/app/modules/auth/auth.service.ts
+++ src/app/modules/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { IUserLogin, IUserLoginResponse } from './auth.interface'
+import {
+  IRefreshTokenResponse,
+  IUserLogin,
+  IUserLoginResponse,
+} from './auth.interface'
 import { User } from '../user/user.model'
 import httpStatus from 'http-status'
 import ApiError from '../../errors/ApiError'
@@ -8,7 +12,6 @@ import { Secret } from 'jsonwebtoken'
 
 const loginUser = async (payload: IUserLogin): Promise<IUserLoginResponse> => {
   const { email, password } = payload
-  console.log(email, password)
 
   const isUserExists = await User.isUserExists(email)
   if (!isUserExists) {
@@ -39,6 +42,37 @@ const loginUser = async (payload: IUserLogin): Promise<IUserLoginResponse> => {
   }
 }
 
+const refreshTokenService = async (
+  token: string,
+): Promise<IRefreshTokenResponse> => {
+  let verifiedToken = null
+  try {
+    verifiedToken = jwtHelpers.verifyToken(
+      token,
+      config.jwt.refresh_secret as Secret,
+    )
+  } catch (error) {
+    throw new ApiError(httpStatus.FORBIDDEN, 'Invalid Refresh Token')
+  }
+
+  const { email } = verifiedToken
+  const isUserExists = await User.isUserExists(email)
+  if (!isUserExists) {
+    throw new ApiError(httpStatus.NOT_FOUND, 'User does not exist')
+  }
+  const newAccessToken = jwtHelpers.createToken(
+    {
+      email: isUserExists.email,
+    },
+    config.jwt.secret as Secret,
+    config.jwt.secret_expire_in as string,
+  )
+  return {
+    accessToken: newAccessToken,
+  }
+}
+
 export const AuthValidationService = {
   loginUser,
+  refreshTokenService,
 }
